99361d28f2fbbd6eb315db3d091857bccb525f36
Kevin Roy <kevinroy.ai23@bmsce.ac.in>
2025-03-12 15:35:14 +0530

    Add files via upload

---
+++ ai082-1b/src/chessboard082.java
@@ -0,0 +1,16 @@
+public class chessboard082 {
+
+    public static void main(String[] args) {
+        int size = 8;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if ((row + col) % 2 == 0) {
+                    System.out.print("W ");
+                } else {
+                    System.out.print("B ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
